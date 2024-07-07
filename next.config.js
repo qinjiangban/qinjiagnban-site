83a46a240bd7d6ac4e548216d6f97f2584a5ce03
qinjiangban <tanjiangban@gmail.com>
2024-07-08 01:39:41 +0800

    更新 next.config.js

--- next.config.js
+++ next.config.js
@@ -5,10 +5,10 @@ const withNextra = require('nextra')({
 })
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+/*   output: 'export',
   images: {
     unoptimized: true,
-  },
+  }, */
 }
 module.exports = withNextra(nextConfig)
 
